afd105adfd1a2cd05bb79e9a400b880858a4b017
Elena <bond.elena.15.05@gmail.com>
2024-01-11 17:09:34 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,23 +2,19 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float result = value / 70;
-        return result;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        float result = value / 60;
-        return result;
+        return value / 60;
     }
 
     public static float euroToRuble(float value) {
-        float result = value * 70;
-        return result;
+        return value * 70;
     }
 
     public static float dollarToRuble(float value) {
-        float result = value * 60;
-        return result;
+        return value * 60;
     }
 
     public static void main(String[] args) {
@@ -30,5 +26,25 @@ public class Converter {
         System.out.println("140 rubles are " + dollar + " dollars.");
         System.out.println("2 euros are " + rubleFromEuro + " rubles.");
         System.out.println("3 dollars are " + rubleFromDollar + " rubles.");
+
+        float rubles = 420;
+        float euros = 6;
+        float dollars = 7;
+
+        float outputRubleToEuro = Converter.rubleToEuro(rubles);
+        boolean passedRubleToEuro = euros == outputRubleToEuro;
+        System.out.println("420 rubles are 6 euros. Test result : " + passedRubleToEuro);
+
+        float outputRubleToDollar = Converter.rubleToDollar(rubles);
+        boolean passedRubleToDollar = dollars == outputRubleToDollar;
+        System.out.println("420 rubles are 7 euros. Test result : " + passedRubleToDollar);
+
+        float outputEuroToRuble = Converter.euroToRuble(euros);
+        boolean passedEuroToRuble = rubles == outputEuroToRuble;
+        System.out.println("6 euros are 420 rubles. Test result : " + passedEuroToRuble);
+
+        float outputDollarToRuble = Converter.dollarToRuble(dollars);
+        boolean passedDollarToRuble = rubles == outputDollarToRuble;
+        System.out.println("7 dollars are 420 rubles. Test result : " + passedDollarToRuble);
     }
 }
