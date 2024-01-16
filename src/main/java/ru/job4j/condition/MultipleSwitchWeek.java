2ab01994685d291592bee03682dbf9c59bcf280d
Elena <bond.elena.15.05@gmail.com>
2024-01-16 20:49:44 +0300

    Switch новый и старый синтаксис.

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -1,5 +1,8 @@
 package ru.job4j.condition;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
         return switch (name) {
@@ -12,5 +15,47 @@ public class MultipleSwitchWeek {
             case "Воскресенье", "Sunday" -> 7;
             default -> -1;
         };
+    }
+
+    public String sanity(String ternaryBool) {
+        switch (ternaryBool) {
+            case "TRUE":
+                return "sane";
+            default :
+                return "This is ridiculous!";
+        }
+    }
+
+    public String sanity2(String ternaryBool) {
+        String result = switch (ternaryBool) {
+            case "TRUE", "FALSE" -> {
+                yield "sane"; }
+            default -> {
+                yield "This is ridiculous!"; }
+        };
+        return result;
+    }
+
+    //старый синтаксис
+    public boolean sanity3(String ternaryBool) throws IOException {
+        boolean result;
+        switch (ternaryBool) {
+            case "TRUE":
+                result = true;
+                break;
+            case "FALSE":
+                result = false;
+                break;
+            case "FILE_NOT_FOUND":
+                // объявление переменной для демонстрации проблемы в default
+                var ex = new IOException("This is ridiculous!",
+                        new FileNotFoundException());
+                throw ex;
+            default:
+                // А вот и проблема: мы не можем объявить еще одну переменную с именем ex
+                var ex2 = new IllegalArgumentException("Seriously?!");
+                throw ex2;
+        }
+        return result;
     }
 }
