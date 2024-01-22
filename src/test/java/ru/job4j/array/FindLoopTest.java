c0b29b413a91f3405bc7e533111ff2944c4394b3
Elena <bond.elena.15.05@gmail.com>
2024-01-22 17:10:59 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class FindLoopTest {
     @Test
-    void whenArrayHas5Then0() {
+    void whenArrayHas5InIndexOfThen0() {
         int[] data = new int[] {5, 10, 3};
         int element = 5;
         int actual = FindLoop.indexOf(data, element);
@@ -14,11 +14,33 @@ class FindLoopTest {
     }
 
     @Test
-    void whenArrayHasNot10ThenMinus1() {
+    void whenArrayHasNot10InIndexOfThenMinus1() {
         int[] data = new int[] {5, 10, 3};
         int element = 18;
         int actual = FindLoop.indexOf(data, element);
         int expected = -1;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8InIndexInRangeThenMinus1() {
+        int[] data = new int[] {1, 3, 5, 8};
+        int element = 8;
+        int start = 0;
+        int end = 2;
+        int actual = FindLoop.indexInRange(data, element, start, end);
+        int expected = -1;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas8InEndInIndexInRangeThen3() {
+        int[] data = new int[] {1, 3, 5, 8};
+        int element = 8;
+        int start = 0;
+        int end = 3;
+        int actual = FindLoop.indexInRange(data, element, start, end);
+        int expected = 3;
         assertThat(actual).isEqualTo(expected);
     }
 }
