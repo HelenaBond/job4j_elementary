f0448cd6c780199edd2e290172de43da12c8ee08
Elena <bond.elena.15.05@gmail.com>
2024-01-24 13:37:44 +0300

    Refactoring. 6.7.4. Выигрышные комбинации в сокобан [#53859 #475897]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -34,11 +34,12 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        char[] diagonal = extractDiagonal(board);
-        for (int i = 0; i < diagonal.length; i++) {
-            if (diagonal[i] == 'X' && (monoVertical(board, i) || monoHorizontal(board, i))) {
-                result = true;
-                break;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'X' && (monoVertical(board, i) || monoHorizontal(board, i))) {
+                    result = true;
+                    break;
+                }
             }
         }
         return result;
