e80887f960c46fde8cf0fb96569866859a05e093
Elena <bond.elena.15.05@gmail.com>
2024-02-01 15:57:59 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,45 +7,37 @@ class PointTest {
 
     @Test
     void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double actual = a.distance(b);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double actual = Point.distance(x1, y1, x2, y2);
         assertThat(actual).isEqualTo(expected, offset(0.01));
     }
 
     @Test
     void when10to20then2() {
+        Point a = new Point(1, 0);
+        Point b = new Point(2, 0);
+        double actual = a.distance(b);
         double expected = 1;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double actual = Point.distance(x1, y1, x2, y2);
         assertThat(actual).isEqualTo(expected, offset(0.01));
     }
 
     @Test
     void when0Minus1to20then2Dot23() {
+        Point a = new Point(0, -1);
+        Point b = new Point(2, 0);
+        double actual = a.distance(b);
         double expected = 2.23;
-        int x1 = 0;
-        int y1 = -1;
-        int x2 = 2;
-        int y2 = 0;
-        double actual = Point.distance(x1, y1, x2, y2);
         assertThat(actual).isEqualTo(expected, offset(0.01));
     }
 
     @Test
     void when00to23then3Dot6() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 3);
+        double actual = a.distance(b);
         double expected = 3.6;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 3;
-        double actual = Point.distance(x1, y1, x2, y2);
         assertThat(actual).isEqualTo(expected, offset(0.01));
     }
 }
